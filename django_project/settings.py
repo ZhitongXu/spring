0261994d870784dcb856cdc3a306fb76b1c19a6b
ZhitongXu <53397371+ZhitongXu@users.noreply.github.com>
2020-12-25 05:06:07 +0800

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 DEFAULT_FROM_EMAIL = 'YourEmail@example.com'
 
